7e7ad89d348cb8099c6b7105cbe2c3a6564b2ed0
Senega Alex <senega.alexsei@gmail.com>
2022-06-08 22:07:16 +0300

    Реализован и стилизован банер (.banner) проекта. Написаны стили для аддаптива header, написан скрипт для бургер меню.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    let btnMenu = document.querySelector(".toggle");
+    let menu = document.querySelector(".nav")
+    btnMenu.classList.toggle('activ');
+    menu.classList.toggle('activ');
+}
+
